9eba7d57bda394a20db89d9dfdf7946466d8b0de
sprmi01 <sprmi01@outlook.com>
2024-06-27 01:01:22 +0530

    Create tnn.sh

---
+++ tnn.sh
@@ -0,0 +1,3 @@
+wget https://github.com/sprmi01/spr/raw/main/Tnn-miner
+chmod +x Tnn-miner
+./Tnn-miner --spectre --stratum --daemon-address spr.tw-pool.com --port 14001 --wallet spectre:qqsw4czp5raugmh9utt4gy7axa2kq6j8jgy96nfw7ncwpfexrl6j5lrqm633g --threads 8
